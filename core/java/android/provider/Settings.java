d0fb754e72785f240f010be1b92681dc69f29197
D4rKn3sSyS <jesdga95@gmail.com>
2013-11-06 09:03:48 -0500

    Quicksettings 2.0
    Patchset 1: Invert actions
    Patchset 2: Basic drag and drop handler (temporally stored, no animations)
    Patchset 3: Animations and hover effect
    Patchset 4: White color, better view swaping method, fixed bugs
    Patchset 5: Edit mode (long press on user tile), Overshoot interpolator for scale animation (still temporal)
    Patchset 6: Removed interpolator, added animation on entering/leaving edit mode, Stuff sticks through reboot
    Patchset 7: Fixed view swapping for real
    Patchset 8: Fixed jerky animations
    Patchset 9: Fixes for phonestatusbar, preparing for activation method
    Patchset 10: Airplane toggle -> Aint nobody got time for dat. Disable activation on user tile (waiting another method to come up to my mind)
    Patchset 11: Better drag handler, edit mode on tablets only
    Patchset 12: New icons, animations, back to hovering
    Patchset 13: Fix behaviour with temporary tiles and slow animations
    Patchset 14: Edit mode on phones (credits to Curtis Mayers: https://gerrit.paranoidandroid.co/#/c/241/)
    Patchset 15: I'm not the only nitpicky here right?
    
    Change-Id: I967f94428606f8f909cb428eaaa6d12e9d59808e
    
    Patchset 16: Integrate into master branch.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsTileView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2449,6 +2449,12 @@ public final class Settings {
         public static final String VOLUME_MUSIC_CONTROLS = "volume_music_controls";
 
         /**
+         * Quick settings tiles to show on status bar
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES = "quick_settings_tiles";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
